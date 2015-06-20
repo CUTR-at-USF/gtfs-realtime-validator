bf9f89562482867f843bc07dda513abfe12ed676
nipuna777 <nipuna777@gmail.com>
2015-06-21 03:42:33 +0530

    Changed apache licence headers

--- src/main/java/edu/usf/cutr/gtfsrtvalidator/servlets/TriggerBackgroundServlet.java
+++ src/main/java/edu/usf/cutr/gtfsrtvalidator/servlets/TriggerBackgroundServlet.java
@@ -1,3 +1,17 @@
+/*
+ ***********************************************************************************************************************
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ ***********************************************************************************************************************
+*/
+
 package edu.usf.cutr.gtfsrtvalidator.servlets;
 
 import edu.usf.cutr.gtfsrtvalidator.background.BackgroundSingleton;
@@ -8,9 +22,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-/**
- * Created by nipuna on 6/17/15.
- */
 public class TriggerBackgroundServlet extends HttpServlet {
 
     @Override
