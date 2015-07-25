adc2c74bc657c738246d76df713db1a7d95a4cc7
nipuna <nipuna777@gmail.com>
2015-07-25 19:10:44 +0530

    Added GTFS-RT to Restful

---
+++ src/main/java/edu/usf/cutr/gtfsrtvalidator/api/resource/GtfsRtFeed.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2011 Nipuna Gunathilake.
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package edu.usf.cutr.gtfsrtvalidator.api.resource;
+
+public class GtfsRtFeed {
+
+    //TODO: GET feed to retrive all rt-feeds
+    //TODO: POST add new gtfs-rt feed to monitored list
+
+    //INFO: @Path("{id : \\d+}") //support digit only
+    //TODO: GET {id} return information about the feed with {id}
+    //TODO: DELETE {id} remove feed with {id}
+}
