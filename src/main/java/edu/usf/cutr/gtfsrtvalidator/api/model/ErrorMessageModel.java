16e72673cf3584491fc0eb2295cadc6bdb6aada7
nipuna <nipuna777@gmail.com>
2015-08-25 07:51:41 +0530

    Changed ErrorMessageModel to contain more details

--- src/main/java/edu/usf/cutr/gtfsrtvalidator/api/model/ErrorMessageModel.java
+++ src/main/java/edu/usf/cutr/gtfsrtvalidator/api/model/ErrorMessageModel.java
@@ -21,7 +21,9 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
 public class ErrorMessageModel {
+    private String type;
     private String message;
+    private String title;
 
     public ErrorMessageModel(){}
 
@@ -35,5 +37,21 @@ public class ErrorMessageModel {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 }
