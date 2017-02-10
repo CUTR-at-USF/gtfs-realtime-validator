cf63e54713cf4f88110affd980cdb46b742e0016
mohangandhiGH <mohangandhi@mail.usf.edu>
2017-02-09 19:13:28 -0500

    Fix #53 - Writing Unit tests for gtfs-realtime validation functions

---
+++ src/test/java/edu/usf/cutr/gtfsrtvalidator/FeedMessageTest.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (C) 2017 University of South Florida.
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.usf.cutr.gtfsrtvalidator;
+
+import com.google.transit.realtime.GtfsRealtime;
+import edu.usf.cutr.gtfsrtvalidator.helper.ErrorListHelperModel;
+import edu.usf.cutr.gtfsrtvalidator.test.headerandentity.FeedEntityTest;
+import edu.usf.cutr.gtfsrtvalidator.test.headerandentity.FeedHeaderTest;
+import java.io.File;
+import java.io.IOException;
+import junit.framework.TestCase;
+import org.onebusaway.gtfs.impl.GtfsDaoImpl;
+import org.onebusaway.gtfs.serialization.GtfsReader;
+import org.onebusway.gtfs_realtime.exporter.GtfsRealtimeLibrary;
+
+
+public class FeedMessageTest extends TestCase {
+    
+    public GtfsDaoImpl gtfsData, gtfsData2; // gtfsData2 contains location_type = 1 for stop_id
+    public GtfsReader reader;
+    public final File staticGtfs = new File("src/test/resources/testagency.zip");
+    public final File staticGtfs2 = new File("src/test/resources/testagency2.zip");
+    public final static String ENTITY_ID = "TEST_ENTITY";
+    
+    FeedHeaderTest feedHeaderTest;
+    FeedEntityTest feedEntityTest;
+    
+    public ErrorListHelperModel errors;
+    
+    public GtfsRealtime.FeedMessage.Builder feedMessageBuilder;
+    public GtfsRealtime.FeedEntity.Builder feedEntityBuilder;
+    public GtfsRealtime.FeedHeader.Builder feedHeaderBuilder;
+    
+    public GtfsRealtime.TripUpdate.Builder tripUpdateBuilder;
+    public GtfsRealtime.VehiclePosition.Builder vehiclePositionBuilder;
+    public GtfsRealtime.Alert.Builder alertBuilder;
+        
+    public FeedMessageTest() throws IOException {
+        errors = new ErrorListHelperModel();
+        
+        feedMessageBuilder = GtfsRealtimeLibrary.createFeedMessageBuilder();
+        feedEntityBuilder = GtfsRealtime.FeedEntity.newBuilder();
+        feedHeaderBuilder = GtfsRealtime.FeedHeader.newBuilder();
+
+        tripUpdateBuilder = GtfsRealtime.TripUpdate.newBuilder();
+        vehiclePositionBuilder = GtfsRealtime.VehiclePosition.newBuilder();
+        alertBuilder = GtfsRealtime.Alert.newBuilder();
+        
+        // Read GTFS data into a GtfsDaoImpl
+        gtfsData = new GtfsDaoImpl();
+        reader = new GtfsReader();
+        reader.setInputLocation(staticGtfs);
+        reader.setEntityStore(gtfsData);
+        reader.run();
+            
+        gtfsData2 = new GtfsDaoImpl();
+        reader = new GtfsReader();
+        reader.setInputLocation(staticGtfs2);
+        reader.setEntityStore(gtfsData2);
+        reader.run();
+        
+        clearAndInitRequiredFeedFields();
+    }
+    
+    // Initialization of some required fields in FeedMessage
+    public final void clearAndInitRequiredFeedFields() {
+        // clear the fields first
+        feedEntityBuilder.clear();
+        feedMessageBuilder.clear();
+        feedHeaderBuilder.clear();
+        
+        tripUpdateBuilder.clear();
+        vehiclePositionBuilder.clear();
+        alertBuilder.clear();
+        
+        feedHeaderBuilder.setGtfsRealtimeVersion("1.0");
+        feedMessageBuilder.setHeader(feedHeaderBuilder);
+        
+        feedEntityBuilder.setId(ENTITY_ID);
+        feedMessageBuilder.addEntity(feedEntityBuilder);
+    }
+    
+    /**
+     * Tests the FeedHeader and FeedEntity. Initializing object of FeedHeader and FeedEntity will execute the
+     * tests in them.
+     * @throws Exception
+     */
+    public final void testMessageFeed() throws Exception {
+        feedHeaderTest = new FeedHeaderTest();
+        feedEntityTest = new FeedEntityTest();
+    }
+}
---
+++ src/test/java/edu/usf/cutr/gtfsrtvalidator/test/feeds/combined/TripUpdateVehiclePostionAlertTest.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (C) 2017 University of South Florida.
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.usf.cutr.gtfsrtvalidator.test.feeds.combined;
+
+import com.google.transit.realtime.GtfsRealtime;
+import edu.usf.cutr.gtfsrtvalidator.FeedMessageTest;
+import edu.usf.cutr.gtfsrtvalidator.validation.entity.LocationTypeReferenceValidator;
+import static junit.framework.TestCase.assertEquals;
+import org.junit.Test;
+
+/*
+ * Tests all the warnings and rules that validate TripUpdate, VehiclePosition and Alerts feed.
+ * Tests: 0e11 - "All stop_ids referenced in GTFS-rt feeds must have the 'location_type' = 0"
+*/
+public class TripUpdateVehiclePostionAlertTest extends FeedMessageTest {
+    
+    public TripUpdateVehiclePostionAlertTest() throws Exception {}
+    
+    @Test
+    public void testLocationTypeValidation() {
+        
+        LocationTypeReferenceValidator locationValidator = new LocationTypeReferenceValidator();
+        
+        GtfsRealtime.TripDescriptor.Builder tripDescriptorBuilder = GtfsRealtime.TripDescriptor.newBuilder();
+        GtfsRealtime.TripUpdate.StopTimeUpdate.Builder stopTimeUpdateBuilder = GtfsRealtime.TripUpdate.StopTimeUpdate.newBuilder();
+        GtfsRealtime.EntitySelector.Builder entitySelectorBuilder = GtfsRealtime.EntitySelector.newBuilder();
+        
+        // tripDescriptor is a required field in tripUpdate
+        tripUpdateBuilder.setTrip(tripDescriptorBuilder.build());
+        feedEntityBuilder.setTripUpdate(tripUpdateBuilder.build());
+        feedMessageBuilder.setEntity(0, feedEntityBuilder.build());
+        
+        // setting stop id = "A" in all the three feeds that matches the stop id in static Gtfs data
+        stopTimeUpdateBuilder.setStopId("A");
+        tripUpdateBuilder.addStopTimeUpdate(stopTimeUpdateBuilder.build());
+        feedEntityBuilder.setTripUpdate(tripUpdateBuilder.build());
+        feedMessageBuilder.setEntity(0, feedEntityBuilder.build());
+        
+        vehiclePositionBuilder.setStopId("A");
+        feedEntityBuilder.setVehicle(vehiclePositionBuilder.build());
+        feedMessageBuilder.setEntity(0, feedEntityBuilder.build());
+        
+        entitySelectorBuilder.setStopId("A");
+        alertBuilder.addInformedEntity(entitySelectorBuilder.build());
+        feedEntityBuilder.setAlert(alertBuilder.build());
+        feedMessageBuilder.setEntity(0, feedEntityBuilder.build());
+        
+        // all the feeds have valid stop id matching that in static Gtfs data. So, returns 0 errors
+        errors = locationValidator.validate(gtfsData, feedMessageBuilder.build());
+        assertEquals(0, errors.getOccurrenceList().size());
+        
+        // setting stop id = "DUMMY" in TripUpdate feed that does not match with any stop id in static Gtfs data
+        stopTimeUpdateBuilder.setStopId("DUMMY");
+        tripUpdateBuilder.addStopTimeUpdate(stopTimeUpdateBuilder.build());
+        feedEntityBuilder.setTripUpdate(tripUpdateBuilder.build());
+        feedMessageBuilder.setEntity(0, feedEntityBuilder.build());
+        errors = locationValidator.validate(gtfsData, feedMessageBuilder.build());
+        // one error from TripUpdate feed stop_id = "DUMMY". VehiclePosition and Alert feeds have valid stop id = "A"
+        assertEquals(1, errors.getOccurrenceList().size());
+        
+        vehiclePositionBuilder.setStopId("DUMMY");
+        feedEntityBuilder.setVehicle(vehiclePositionBuilder.build());
+        feedMessageBuilder.setEntity(0, feedEntityBuilder.build());
+        errors = locationValidator.validate(gtfsData, feedMessageBuilder.build());
+        // 2 errors from TripUpdate and VehiclePosition feeds stop_id="DUMMY". Alert feed have valid stop id ="A"
+        assertEquals(2, errors.getOccurrenceList().size());
+        
+        entitySelectorBuilder.setStopId("DUMMY");
+        alertBuilder.addInformedEntity(entitySelectorBuilder.build());
+        feedEntityBuilder.setAlert(alertBuilder.build());
+        feedMessageBuilder.setEntity(0, feedEntityBuilder.build());
+        errors = locationValidator.validate(gtfsData, feedMessageBuilder.build());
+        // 3 errors from TripUpdate, VehiclePosition and alert feeds stop_id="DUMMY"
+        assertEquals(3, errors.getOccurrenceList().size());
+        
+        clearAndInitRequiredFeedFields();
+    }    
+}
