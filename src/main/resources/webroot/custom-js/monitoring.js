9ca63d6062cd8a7462b7a31c2220a60e12c617c9
mohangandhiGH <mohangandhi@mail.usf.edu>
2017-06-24 13:40:00 -0400

    Fix #215 - Support multiple clients with simultaneous feeds monitoring

--- src/main/resources/webroot/custom-js/monitoring.js
+++ src/main/resources/webroot/custom-js/monitoring.js
@@ -50,6 +50,7 @@ for (var gtfsRtFeed in gtfsRtFeeds) {
             type: 'PUT',
             success: function (data) {
                 initializeInterface(data["gtfsRtFeedModel"]);
+                sessionStorage.setItem("sessionStartTime", data["sessionStartTime"]);
                 refresh(data["gtfsRtFeedModel"]["gtfsRtId"]);
 
                 // SessionId's for each of the GTFS-rt-feed. On 'stop' monitoring feeds, 'Session' table 'sessionEndTime' is updated using these sessionId's.
@@ -78,7 +79,8 @@ function loadGtfsErrorCount(gtfsFeedId) {
 function refresh(id) {
 
     $.get(server + "/api/gtfs-rt-feed/monitor-data/" + id +
-            "?summaryCurPage=" + paginationSummary[id]["currentPage"] +
+            "?startTime=" + sessionStorage.getItem("sessionStartTime") +
+            "&summaryCurPage=" + paginationSummary[id]["currentPage"] +
             "&summaryRowsPerPage=" + paginationSummary[id]["rowsPerPage"] +
             "&toggledData=" + hideErrors[id] +
             "&logCurPage=" + paginationLog[id]["currentPage"] +
