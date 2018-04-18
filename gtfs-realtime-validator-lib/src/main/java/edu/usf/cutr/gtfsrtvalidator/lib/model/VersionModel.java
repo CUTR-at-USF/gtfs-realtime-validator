ebbb9fb678986613bf0a98f8c5866bd921516a48
barbeau <barbeau@cutr.usf.edu>
2018-04-18 14:05:25 -0400

    Revert accidental change to license header comments

--- gtfs-realtime-validator-lib/src/main/java/edu/usf/cutr/gtfsrtvalidator/lib/model/VersionModel.java
+++ gtfs-realtime-validator-lib/src/main/java/edu/usf/cutr/gtfsrtvalidator/lib/model/VersionModel.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2018 University of South Florida (sjbarbeau@gmail.com)
  *
- * Licensed under the Apache License, VersionModel 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
